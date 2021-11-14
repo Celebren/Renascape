4ae8569da19e241777212288c70eafa0049bf442
Rena <rena.s.silver@gmail.com>
2021-11-14 12:53:20 +0000

    MySqli Facade
    Implemented facade design pattern for mysqli with "select", "inset" and "delete all" methods

--- php/classes/Database.php
+++ php/classes/Database.php
@@ -1,31 +1,87 @@
 <?php
 namespace Database;
 
+
+/**
+ * Facade class for mysqli queries
+ */
+
 Class Database {
-	public static function connector(string $sType) {
-		$servername = "localhost";
-		$username = "username";
-		$password = "password";
-		$dbname = "myDB";
+	private static $conn = null;
+
+	private static function connector() {
+		$sServer = "localhost";
+		$sUser = "root";
+		$password = "";
+		$sDb = "renascape";
 
 		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-		}
+		self::$conn = new \mysqli($sServer, $sUser, $password, $sDb);
 
-		$sql = "SELECT id, firstname, lastname FROM MyGuests";
-		$result = $conn->query($sql);
+		// Check connection
+		if (self::$conn->connect_error) {
+			die("Connection failed: " . self::$conn->connect_error);
+		}
+	}
+
+	/**
+	 * Execute select query and return associative array of results
+	 * @param string $sql query to execute
+	 * @return array array of results. Empty if no results found
+	 */
+	public static function select(string $sql) : array {
+		self::connector();
+		$rows = [];
+
+		$result = self::$conn->query($sql);
 
 		if ($result->num_rows > 0) {
-		// output data of each row
-		while($row = $result->fetch_assoc()) {
-			echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-		}
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				$rows[] = $row;
+			}
 		} else {
-		echo "0 results";
+			error_log("Error selecting from database: " . self::$conn->error);
 		}
-		$conn->close();
+
+		self::$conn->close();
+		return $rows;
+	}
+
+	/**
+	 * Execute insert query and return the last ID inserted
+	 * @param string $sql query to execute
+	 * @return int ID of last row inserted. 0 if none inserted
+	 */
+	public static function insert(string $sql) : int {
+		self::connector();
+		$id = 0;
+
+		if (self::$conn->query($sql) === TRUE) {
+			$id = self::$conn->insert_id;
+		} else {
+			error_log("Error inserting to database: " . self::$conn->error);
+		}
+
+		self::$conn->close();
+		return $id;
+	}
+
+	/**
+	 * Delete all entries in a table
+	 * @param string $sTable table name
+	 * @return void
+	 */
+	public static function deleteAll(string $sTable) : void {
+		self::connector();
+		$sql = "DELETE FROM $sTable";
+
+		if (self::$conn->query($sql) === TRUE) {
+			error_log("Deleted all entries in $sTable");
+		} else {
+			error_log("Error deleting record: " . self::$conn->error);
+		}
+		
+		self::$conn->close();
 	}
 }
--- php/loadMonsters.php
+++ php/loadMonsters.php
@@ -1,6 +1,11 @@
 <?php
 
+require_once('classes/Database.php');
+
+use Database\Database;
+
 // 1. Open Monster JSON
 $sFile = file_get_contents("../data/bestiary-mm.json");
 $aData = json_decode($sFile, true);
-// error_log(print_r($aData, true));
+$rows = Database::select("SELECT * FROM monster");
+// error_log(print_r($rows, true));
